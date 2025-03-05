9cdc6539bb386b97d15e45fac59e3ea34dae8580
Zerh <83118305+Zerahu@users.noreply.github.com>
2025-03-05 21:32:35 +0100

    Initial commit, wrote README

---
+++ README.md
@@ -0,0 +1,27 @@
+# badlion-client-installer
+
+Simple bash script that automatically installs badlionclient on (Arch) Linux
+
+## How to use:
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/Zerahu/badlion-client-installer.git
+```
+
+2. Enter the directory:
+
+```bash
+cd badlion-client-installer
+```
+
+3. Run the install script:
+
+```
+./install.sh
+```
+
+### Disclaimers
+
+I think the .desktop file implementation is slightly broken, might fix that in the future. Feel free to create pull requests. No warranty on this as I am no longer associated with Badlion client. This project is made externally without explicit consent from Badlion Client.
---
+++ install.sh
@@ -0,0 +1,38 @@
+echo "Script requires sudo priviledges: "
+sudo echo " "
+
+if ! command -v wget &>/dev/null; then
+  echo "wget is not installed. Make sure to install it before running this script!"
+  exit 1
+fi
+
+wget -O BadlionClient "https://client-updates-cdn77.badlion.net/BadlionClient"
+
+
+if [ -f "BadlionClient" ]; then
+    chmod a+x BadlionClient
+    sudo mv BadlionClient /usr/bin
+
+    sudo wget -O /usr/share/icons/badlion-logo.png "https://assets.badlion.net/cdn-cgi/image/width=300,f=auto/site/assets/badlion-logo.webp"
+
+    echo """
+    [Desktop Entry]
+    Name=Badlion Client
+    StartupWMClass=Badlion Client
+    Comment=Badlion Client
+    GenericName=Minecraft Client Badlion pvp
+    Exec=/usr/bin/BadlionClient %U
+    Icon=/usr/share/icons/badlion-icon.png
+    Type=Application
+    StartupNotify=true
+    Categories=Minecraft;Gaming;Client;Modpack;
+    """ > BadlionClient.desktop
+
+    sudo mv BadlionClient.desktop /usr/share/applications/BadlionClient.desktop
+
+    exit 0
+
+else
+    echo "Make sure to navigate to the folder where badlion is located first!"
+    exit 1
+fi
